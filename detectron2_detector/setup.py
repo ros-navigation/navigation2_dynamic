6acfa79026c8346f4382e3fbf7012d07da084d47
shengjian.chen <csj15thu@gmail.com>
2020-10-13 00:07:04 -0700

    small changes

--- detectron2_detector/setup.py
+++ detectron2_detector/setup.py
@@ -5,7 +5,7 @@ package_name = 'detectron2_detector'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='0.0.1',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
@@ -16,10 +16,10 @@ setup(
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='tony',
-    maintainer_email='csj15thu@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    maintainer='Shengjian Chen, Steven Macenski',
+    maintainer_email='csj15thu@gmail.com, stevenmacenski@gmail.com',
+    description='This detector uses detectron2 to get object mask and then uses pointcloud2 data to estimate 3D position.',
+    license='Apache-2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
